d52204866db3f89a356c6d4be70d035dce427139
Priyan Chandrapala <priyanchandrapala@Priyans-MacBook-Pro.local>
2023-11-13 19:02:53 +0000

    added functions for jpg filtering and exporting to hive tables to hdfs

--- src/main/scala/commoncrawl/base/ExtractWatContents.scala
+++ src/main/scala/commoncrawl/base/ExtractWatContents.scala
@@ -3,7 +3,7 @@ package commoncrawl.base
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark
 import org.apache.spark.sql.SaveMode
-import org.apache.spark.sql.functions.explode
+import org.apache.spark.sql.functions.{col, explode}
 
 case class HiveTable(hiveTableName: String, hiveTablePath: String)
 
@@ -20,17 +20,28 @@ object ExtractWatContents {
     val htmlResponseMetadataDf = payloadDF.select("Envelope.Payload-Metadata.HTTP-Response-Metadata.HTML-Metadata")
     val linksDf = htmlResponseMetadataDf.select("HTML-Metadata.Links")
     val explodedLinksDf = linksDf.select(explode($"Links")).as("exploded_links").select("exploded_links.*")
-
-    explodedLinksDf
+    val flattenedDF = explodedLinksDf.select(
+      col("col.path").alias("path"),
+      col("col.target").alias("target"),
+      col("col.text").alias("text"),
+      col("col.title").alias("title"),
+      col("col.url").alias("url")
+    )
+    flattenedDF
 
   }
 
   def createHiveTableIfNotExist(fullyQualifiedTableName: String, hiveTablePath: String, df: DataFrame): Unit = {
-       df.write.mode(SaveMode.Overwrite).option("path", hiveTablePath)saveAsTable(fullyQualifiedTableName)
+       df.write.mode(SaveMode.Overwrite)
+         .format("parquet")
+         .option("path", hiveTablePath)
+         .saveAsTable(fullyQualifiedTableName)
   }
 
   private def appendDataframeToHiveTable(fullyQualifiedTableName: String, df: DataFrame): Unit ={
-    df.write.mode(SaveMode.Append)saveAsTable(fullyQualifiedTableName)
+    df.write.mode(SaveMode.Append)
+      .format("parquet")
+      .saveAsTable(fullyQualifiedTableName)
   }
 
   def insertOrAppendToHiveTable( dataFrame: DataFrame)(hiveTable: HiveTable)(implicit spark: SparkSession): Unit = {
@@ -97,5 +108,49 @@ object ExtractWatContents {
     }
     processedFiles
   }
+
+ def  extractJpgJpegFromAllUrls(explodedRawWatFilesHiveTable:HiveTable,jpgUrlHiveTable:HiveTable)(implicit spark:SparkSession): Unit = {
+   val dfRawUnfilteredUrls= spark.read.table(explodedRawWatFilesHiveTable.hiveTableName)
+   val jpgUrlsDF = dfRawUnfilteredUrls
+                            .filter(dfRawUnfilteredUrls("url").isNotNull)
+                            .where("lower(url) like 'http%.jpg'")
+
+   val jpegUrlsDF = dfRawUnfilteredUrls
+                            .filter(dfRawUnfilteredUrls("url").isNotNull)
+                            .where("lower(url) like 'http%.jpeg'")
+
+   jpgUrlsDF.printSchema()
+
+   insertOrAppendToHiveTable(jpgUrlsDF)(jpgUrlHiveTable)
+   insertOrAppendToHiveTable(jpegUrlsDF)(jpgUrlHiveTable)
+
+ }
+
+def extractPngFromAllUrls(explodedRawWatFilesHiveTable: HiveTable, pngUrlHiveTable: HiveTable)(implicit spark: SparkSession): Unit = {
+    val dfRawUnfilteredUrls = spark.read.table(explodedRawWatFilesHiveTable.hiveTableName)
+    val pngUrlsDF = dfRawUnfilteredUrls
+      .filter(dfRawUnfilteredUrls("url").isNotNull)
+      .where("lower(url) like 'http%.png'")
+
+    insertOrAppendToHiveTable(pngUrlsDF)(pngUrlHiveTable)
+
+}
+
+ def  deduplicateUrls(urlHiveTable: HiveTable, distinctUrlHiveTable: HiveTable)(implicit spark: SparkSession) : Unit = {
+   val dfUrls = spark.read.table(urlHiveTable.hiveTableName)
+   val dfDeduplicatedUrls= dfUrls.dropDuplicates("url")
+
+   insertOrAppendToHiveTable(dfDeduplicatedUrls)(distinctUrlHiveTable)
+
+ }
+
+
+  def exportHiveTableToHdfsCsv(hiveTable: HiveTable, hdfsPath: String)(implicit spark: SparkSession): Unit = {
+    val warcFileListDf = spark.table(hiveTable.hiveTableName)
+    import spark.implicits._
+    warcFileListDf.coalesce(1).write.option("sep", "|").csv(hdfsPath)
+  }
+
+
 
 }
--- src/test/scala/commoncrawl/base/ExtractWATContentsTest.scala
+++ src/test/scala/commoncrawl/base/ExtractWATContentsTest.scala
@@ -2,12 +2,13 @@ package commoncrawl.base
 
 import com.holdenkarau.spark.testing.Prettify.maxNumberOfShownValues
 import com.holdenkarau.spark.testing.SharedSparkContext
-import commoncrawl.base.ExtractWatContents.{createHiveTableIfNotExist, extractWATContentsAsDataframe, getProcessedFileNames, insertOrAppendToHiveTable, listHdfsFiles, processFile, processRawWatFiles}
+import commoncrawl.base.ExtractWatContents.{createHiveTableIfNotExist, deduplicateUrls, exportHiveTableToHdfsCsv, extractJpgJpegFromAllUrls, extractPngFromAllUrls, extractWATContentsAsDataframe, getProcessedFileNames, insertOrAppendToHiveTable, listHdfsFiles, processFile, processRawWatFiles}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark
 
 class ExtractWATContentsTest extends AnyFunSuite with SharedSparkContext{
 
@@ -15,19 +16,38 @@ class ExtractWATContentsTest extends AnyFunSuite with SharedSparkContext{
   var fs: FileSystem = _
   override def beforeAll(): Unit = {
     super.beforeAll()
-    spark = SparkSession.builder().config(sc.getConf).getOrCreate()
+    spark = SparkSession.builder().enableHiveSupport().config(sc.getConf).getOrCreate()
     spark.sql("CREATE DATABASE IF NOT EXISTS test_db LOCATION 'tmp/test_db1.db' ");
     fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
   }
   
-  val rawWatFilePath = "src/test/resources/raw_wat"
+  val rawWatFilePath = "src/test/resources/hdfs_file_system/raw_wat"
   val outputPath = "src/test/resources/output_parquet"
+  val jpgExportPath = "src/test/resources/hdfs_file_system/outputs/jpg_export_path"
+
   val sampleWatFileName = "CC-MAIN-sample.warc.wat.gz"
-  val fullyQualifiedHiveTableName = "test_db.raw_links"
-  val hiveTablePath = "spark-warehouse/tmp/test_db1.db/raw_links"
+
+  val rawLinksFullyQualifiedHiveTableName = "test_db.raw_links"
+  val rawLinksHiveTablePath = "spark-warehouse/tmp/test_db1.db/raw_links"
+
+  val jpgLinksFullyQualifiedHiveTableName = "test_db.jpg_links"
+  val jpgLinksHiveTablePath = "spark-warehouse/tmp/test_db1.db/jpg_links"
+
+  val jpgDistinctLinksFullyQualifiedHiveTableName = "test_db.jpg_distinct_links"
+  val jpgDistinctLinksHiveTablePath = "spark-warehouse/tmp/test_db1.db/jpg_distinct_links"
+
+  val pngLinksFullyQualifiedHiveTableName = "test_db.png_links"
+  val pngLinksHiveTablePath = "spark-warehouse/tmp/test_db1.db/png_links"
+
   val fullWatFilePath = s"$rawWatFilePath/$sampleWatFileName"
-  val processedFilePath = "src/test/resources/processed_file_path"
-  val explodedRawWatFilesHiveTable: HiveTable = HiveTable(fullyQualifiedHiveTableName, hiveTablePath)
+  val processedFilePath = "src/test/resources/hdfs_file_system/outputs/processed_file_path"
+
+  val explodedRawWatFilesHiveTable: HiveTable = HiveTable(rawLinksFullyQualifiedHiveTableName, rawLinksHiveTablePath)
+  val jpgUrlHiveTable: HiveTable = HiveTable(jpgLinksFullyQualifiedHiveTableName, jpgLinksHiveTablePath)
+  val jpgDistinctUrlHiveTable: HiveTable = HiveTable(jpgDistinctLinksFullyQualifiedHiveTableName, jpgDistinctLinksHiveTablePath)
+  val pngUrlHiveTable: HiveTable = HiveTable(pngLinksFullyQualifiedHiveTableName, pngLinksHiveTablePath)
+
+
 
   test("extractWATContents should return a DataFrame from WAT file") {
 
@@ -37,50 +57,96 @@ class ExtractWATContentsTest extends AnyFunSuite with SharedSparkContext{
   }
 
   test("insert hive table with dataframe when the table does not exist") {
-    spark.sql(s"DROP TABLE IF EXISTS $fullyQualifiedHiveTableName ")
+    spark.sql(s"DROP TABLE IF EXISTS $rawLinksFullyQualifiedHiveTableName ")
     val raw_links_path=new Path("spark-warehouse/tmp/test_db1.db/raw_links")
     fs.delete(raw_links_path,true)
 
     val dataFrame = extractWATContentsAsDataframe(fullWatFilePath)
     insertOrAppendToHiveTable(dataFrame)(explodedRawWatFilesHiveTable)
-    spark.sql(s"refresh table $fullyQualifiedHiveTableName")
-
-    assert(spark.read.table(fullyQualifiedHiveTableName).count() > 0)
+    spark.sql(s"refresh table $rawLinksFullyQualifiedHiveTableName")
+    assert(spark.read.table(rawLinksFullyQualifiedHiveTableName).count() > 0)
   }
 
   test("insert hive table with dataframe when the table exist") {
 
     val dataFrame = extractWATContentsAsDataframe(fullWatFilePath)
     insertOrAppendToHiveTable(dataFrame)(explodedRawWatFilesHiveTable)
-    spark.sql(s"refresh table $fullyQualifiedHiveTableName")
-
-    assert(spark.read.table(fullyQualifiedHiveTableName).count() > 0)
+    spark.sql(s"refresh table $rawLinksFullyQualifiedHiveTableName")
+    assert(spark.read.table(rawLinksFullyQualifiedHiveTableName).count() > 0)
   }
 
   test("explode wat file and save to hive table and save the file name in hdfs") {
 
   processFile(fullWatFilePath, processedFilePath)(explodedRawWatFilesHiveTable)
-    spark.sql(s"refresh table $fullyQualifiedHiveTableName")
-
-    assert(spark.read.table(fullyQualifiedHiveTableName).count() > 0)
+    spark.sql(s"refresh table $rawLinksFullyQualifiedHiveTableName")
+    assert(spark.read.table(rawLinksFullyQualifiedHiveTableName).count() > 0)
   }
 
   test("list the number of files in the hdfs folder location") {
-
     val watFiles = listHdfsFiles(rawWatFilePath)
     assert(watFiles.length > 0)
   }
 
   test("Load the set of processed files from a persistent location") {
-
     val processedFileNames = getProcessedFileNames(processedFilePath)
     assert(processedFileNames.nonEmpty)
   }
 
   test("Process files one by one, excluding those already processed") {
-
     val processedFileNames = processRawWatFiles(rawWatFilePath,processedFilePath)(explodedRawWatFilesHiveTable)
     assert(processedFileNames.nonEmpty)
   }
 
+  // need to extract .jpg files from the links
+  test("extract .jpg and .jpeg urls from the table with all urls") {
+    extractJpgJpegFromAllUrls(explodedRawWatFilesHiveTable,jpgUrlHiveTable)
+    assert(spark.read.table(jpgUrlHiveTable.hiveTableName).count() > 0)
+  }
+
+  // need to extract .png files from the links
+  test("extract .png urls from the table with all urls") {
+    extractPngFromAllUrls(explodedRawWatFilesHiveTable, pngUrlHiveTable)
+    assert(spark.read.table(pngUrlHiveTable.hiveTableName).count() > 0)
+  }
+
+  test("deduplicate jpg urls and save them to a table") {
+    deduplicateUrls(jpgUrlHiveTable,jpgDistinctUrlHiveTable);
+    assert(spark.read.table(jpgUrlHiveTable.hiveTableName).count() > spark.read.table(jpgDistinctUrlHiveTable.hiveTableName).count())
+  }
+
+
+  test("test exporting a hive table to hdfs file system") {
+    exportHiveTableToHdfsCsv(jpgDistinctUrlHiveTable, jpgExportPath)
+    val exportFile = listHdfsFiles(jpgExportPath)
+    assert(exportFile.length>0)
+
+  }
+
+  override def afterAll(): Unit = {
+    val jpg_export_path=new Path(jpgExportPath)
+    fs.delete(jpg_export_path,true)
+
+    val output_processed_path = new Path(processedFilePath)
+    fs.delete(output_processed_path, true)
+
+    fs.close()
+  }
+
+
+  def createSparkDfWithUrlCol(implicit spark: SparkSession): DataFrame = {
+    import spark.implicits._
+    // Sample data
+    val data = Seq(
+      ("path1", "target1", "text1", "title1", "http://example.com/page1"),
+      ("path2", "target2", "text2", "title2", "http://example.com/page2"),
+      ("path3", "target3", "text3", "title3", "http://example.com/page1"), // Duplicate URL
+      ("path4", "target4", "text4", "title4", "http://example.com/page3")
+    )
+
+    // Create a DataFrame with the specified schema
+    val df = data.toDF("path", "target", "text", "title", "url")
+    df
+  }
+
 }
+
